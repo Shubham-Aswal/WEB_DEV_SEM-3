e5cbb2f3947194dadd14d9c1782185f0b9e96361
Shubham-Aswal <shubhamaswal1976@gmail.com>
2026-08-16 16:40:26 +0530

    assignment - backend development

---
+++ assignments/Backend Development/index.js
@@ -0,0 +1,68 @@
+import fs from "fs"
+
+// fs.writeFileSync("welcome.txt","Welcome to Backend Development!")
+
+// let data = fs.readFileSync("welcome.txt","utf-8")
+// console.log(data)
+
+
+// fs.writeFileSync("task.txt","Task 1: Learn Node")
+
+// fs.appendFileSync("task.txt","\nTask 2: Learn fs module")
+
+// let data2 = fs.readFileSync("task.txt","utf-8")
+// console.log(data2)
+
+
+
+
+// fs.writeFileSync("demo.txt","AAA")
+// let data3 = fs.readFileSync("demo.txt","utf-8")
+// console.log(data3)
+// fs.writeFileSync("demo.txt","BBB")
+// data3 = fs.readFileSync("demo.txt","utf-8")
+// console.log(data3)
+// fs.appendFileSync("demo.txt","\nCCC")
+// data3 = fs.readFileSync("demo.txt","utf-8")
+// console.log(data3)
+
+
+
+
+
+// fs.mkdirSync("myProject",{recursive : true})
+// fs.writeFileSync("./myProject/info.txt","This is inside a folder")
+// let data4 = fs.readdirSync("myProject")
+// console.log(data4)
+
+
+
+
+
+// console.log("1. Start")
+
+// fs.readFile("welcome.txt","utf-8",(err,data5)=>{
+//     if(!err){
+//         console.log(data5);
+//     }
+//     else{
+//         console.log(err)
+//     }
+// })
+
+// console.log("3. End")
+
+
+
+
+
+// fs.mkdirSync("data")
+// fs.writeFileSync("./data/notes.txt","Note 1: Backend is fun")
+// fs.appendFileSync("./data/notes.txt","\nNote 2: fs module learned\nNote 3: CRUD done")
+// let data6 = fs.readFileSync("./data/notes.txt","utf-8")
+// console.log(data6)
+// fs.rmSync("data",{recursive : true})
+
+
+
+
